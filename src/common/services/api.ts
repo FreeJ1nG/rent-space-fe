39f78c5048a8e23a5ea6caa21fe7c9fcf94291f6
FreeJ1nG <Andrewjeremy12345@gmail.com>
2023-05-25 17:57:26 +0700

    refactor: change api baseUrl

--- src/common/services/api.ts
+++ src/common/services/api.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+    baseUrl: process.env.NEXT_PUBLIC_API_AUTH_BASE_URL,
   }),
   tagTypes: [],
   endpoints: () => ({}),
